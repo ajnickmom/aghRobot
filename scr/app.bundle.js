426ae3c3004c9a9d49dc3af35af96875d8ca4ea2
Pamela Orozco <ajnickmom@gmail.com>
2017-12-15 20:28:56 -0700

    Add files via upload

---
+++ scr/app.bundle.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+   console.log("HELLO WORLD THIS IS BETTER MORE");
+ };
+
+
+/*TODO: Move To js file */
+/*
+/*********************************************************
+ *
+ *   Test Checkboxes/Radio Buttons Script from computers page
+ *
+ *
+ *********************************************************/
+ // loop through each form and handle submit event
+ //for (var i=0;i<document.forms.length;i++) {
+//   var form = document.forms[i];
+//   form.addEventListener('submit', function(e){
+  //   e.preventDefault();
+    // var results = '';
+    // loop through all checkboxes to see if checked
+    //var checkboxes = form.querySelectorAll("input[type='checkbox']");
+   // for(var k = 0; k < checkboxes.length; k++) {
+     // results += checkboxes[k].id + '=' + checkboxes[k].checked + '\n';
+   // }
+    // loop through all radio butotns to display value
+    //var radiobuttons = form.querySelectorAll("input[type='radio']:checked");
+   // for(var m = 0; m < radiobuttons.length; m++) {
+    //  results += radiobuttons[m].name + '=' + radiobuttons[m].value + '\n';
+   // }
+    // alert results
+    //alert(results);
+//  });
+// } /*
